4616ecc1b87c145ef319675d0a0d0018a12b39db
Stanislaw Grams <stanislawgrams@gmail.com>
2021-08-04 23:06:50 +0200

    add desc for syscalls.c _write implementation

--- NonSecure/Core/Src/syscalls.c
+++ NonSecure/Core/Src/syscalls.c
@@ -83,8 +83,8 @@ __attribute__((weak)) int _write(int file, char *ptr, int len)
 
 	for (DataIdx = 0; DataIdx < len; DataIdx++)
 	{
-		//__io_putchar(*ptr++);
-		ITM_SendChar((*ptr++));
+		//__io_putchar(*ptr++); // uncomment for the original implementation
+		ITM_SendChar((*ptr++)); // sends character using SWV ITM data mechanism
 	}
 	return len;
 }
